bf38dd192b2dda3731ebd21856d6464a30f124a0
Philip Wahlbom <philip@wahlbom.net>
2022-02-06 15:33:11 -0500

    Update Program.cs

--- src/ErrorHandling/Program.cs
+++ src/ErrorHandling/Program.cs
@@ -9,6 +9,21 @@ namespace ErrorHandling
     {
         public static void Main(string[] args)
         {
+            Dictionary<int, string> movies = new Dictionary<int, string>();
+            //try
+            //{
+                movies.Add(1, "a");
+                movies.Add(1, "b");
+                movies.Add(2, "c");
+            //}
+            //catch
+            //{
+            //    throw new ArgumentException("Houston, we have a problem.");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("You have reached the 'finally' statement.");
+            //}
         }
     }
 }
